ae0598955b4cb8afa47b31fda98505292478a1c9
keeley <keeley.jun@qq.com>
2018-10-09 19:07:04 +0800

    remove excess

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,4 @@
 import { connect, joinRoom, exitRoom, sendRoomText } from './rongyun';
-window.connect = connect;
-window.joinRoom = joinRoom;
-window.sendRoomText = sendRoomText;
-
 import Vue from 'vue'
 import App from './pages/app.vue'
 
